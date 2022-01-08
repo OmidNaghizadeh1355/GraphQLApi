9923a8c0a3bcbefbdfc67f56e9574d23e74ac189
Omid Naghizadeh <Omid.Naghizadeh.ext@toyota-industries.eu>
2022-01-08 22:10:01 +0100

    Throw erors

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const server = new ApolloServer({
         if(err.extensions.code == 'INTERNAL_SERVER_ERROR'){
             return new ApolloError("Call the Administrator", "ERROR", {token: "uniquetoken"});
         }
+        return err;
     }
 });
 server
